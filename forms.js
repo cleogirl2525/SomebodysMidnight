34cfedce698bc017321c07e4617834fc6e49a2ca
nbriz <nickbriz@gmail.com>
2021-02-07 01:24:42 -0600

    it's midnight

---
+++ forms.js
@@ -0,0 +1,106 @@
+/* global Maths */
+let h = 0
+
+function dancingHearts () {
+  const heart1 = document.querySelector('[alt="question one"]')
+  const heart2 = document.querySelector('[alt="question two"]')
+  const heart3 = document.querySelector('[alt="question three"]')
+  const w3 = window.innerWidth / 3
+  const w2 = window.innerWidth / 2
+  const h2 = window.innerHeight / 2
+
+  h += 0.005 // heart dancing speed
+
+  if (heart1) {
+    heart1.style.top = Math.cos(h) * h2 + h2 + 'px'
+    heart1.style.left = Math.sin(h * 0.6) * w3 + w2 + 'px'
+  }
+
+  if (heart2) {
+    heart2.style.top = Math.sin(h) * w3 + w2 + 'px'
+    heart2.style.left = Math.cos(h * 0.6) * w3 + w2 + 'px'
+  }
+
+  if (heart3) {
+    heart3.style.left = Math.sin(h) * w2 + w2 + 'px'
+  }
+}
+
+function sestina (letter) {
+  [...letter.children].forEach(div => {
+    // split paragraphs into sentances
+    const sents = div.textContent.split('. ')
+    const newSents = []
+    sents.forEach(sent => {
+      // take center 3rd of each setance && randomize
+      let words = sent.split(' ')
+      if (words.length > 3) {
+        const third = Math.ceil(words.length / 3)
+        const begin = words.slice(0, third)
+        let middle = words.slice(third, third * 2)
+        const end = words.slice(third * 2)
+        middle = Maths.shuffle(middle)
+        words = [...begin, ...middle, ...end]
+      }
+      newSents.push(words.join(' '))
+    })
+    if (newSents[newSents.length - 1] === '') newSents.pop()
+    // take second half of sentances && interweave in reverse (from the start)
+    const half = Math.floor(newSents.length / 2)
+    const newSentsA = newSents.slice(0, half)
+    const newSentsB = newSents.slice(half, newSents.length).reverse()
+    const tinad = []
+    const length = newSentsA.length > newSentsB.length ? newSentsA.length : newSentsB.length
+    for (let i = 0; i < length; i++) {
+      if (newSentsB[i]) tinad.push(newSentsB[i])
+      if (newSentsA[i]) tinad.push(newSentsA[i])
+    }
+    div.textContent = tinad.join('. ') + '.'
+    div.textContent = div.textContent.replace('?.', '?')
+    // TODO: ask stackoverflow how to regex ".."
+    // div.textContent = div.textContent.replace(/\../g, '.')
+  })
+}
+
+function countDown () {
+  let h = String(23 - new Date().getHours())
+  let m = String(60 - new Date().getMinutes())
+  let s = String(60 - new Date().getSeconds())
+  if (h.length === 1) h = '0' + h
+  if (m.length === 1) m = '0' + m
+  if (s.length === 1) s = '0' + s
+  return `${h}:${m}:${s}`
+}
+
+function checkTime (interval) {
+  const time = new Date()
+  const hour = time.getHours()
+  const mins = time.getMinutes()
+  const wintime = 10 // window of time (ex: 10mins after 12)
+  if (hour === 0 && mins < wintime) {
+    return 'midnight'
+  } else if (hour === 0 && mins >= wintime) {
+    const blur = Maths.map(mins, wintime, 60, 0, 3)
+    return blur
+  } else {
+    return 'waiting'
+  }
+}
+
+function end (email, subject, body) {
+  document.querySelector('.fb-bottom').style.display = 'none'
+  document.querySelector('.fb-top').style.display = 'none'
+  document.querySelector('.letter').style.display = 'none'
+
+  const data = `mailto:${email}?subject=${subject}&body=${body}`
+  const a = document.createElement('a')
+  a.setAttribute('href', data)
+  a.click()
+  a.remove()
+}
+
+window.sestina = sestina
+window.dancingHearts = dancingHearts
+window.end = end
+window.checkTime = checkTime
+window.countDown = countDown
---
+++ main.js
@@ -0,0 +1,87 @@
+/* global dancingHearts, sestina, end, checkTime, countDown */
+
+// ...................................................................... setup
+
+const email = 'cleogirl2525@gmail.com'
+const subject = 'DEEP SUBJECT'
+const countdown = document.querySelector('.countdown')
+
+const letter = document.querySelector('.letter')
+const first = document.querySelector('.first')
+let t = window.innerHeight / 2
+const margin = window.innerHeight * 0.6
+letter.style.top = `${t}px`
+
+const answers = []
+const questions = [
+  'this is the first question?',
+  'this is the second question?',
+  'this is the third question?'
+]
+
+const submit = document.querySelector('.submit')
+const modal = document.querySelector('.modal')
+const heart1 = document.querySelector('[alt="question one"]')
+const heart2 = document.querySelector('[alt="question two"]')
+const heart3 = document.querySelector('[alt="question three"]')
+
+// ...................................................................... draw
+
+function draw () {
+  setTimeout(draw, 1000 / 30)
+
+  if (checkTime() === 'waiting') {
+    letter.style.filter = 'blur(3px)'
+    countdown.textContent = countDown()
+  } else {
+    if (typeof checkTime() === 'number') {
+      letter.style.filter = `blur(${checkTime()}px)`
+    }
+
+    dancingHearts()
+
+    t -= 0.75 // speed
+    letter.style.top = `${t}px`
+
+    if (-t >= letter.offsetHeight * 0.2625 && heart1.parentElement) {
+      heart1.style.display = 'block'
+    }
+    if (-t >= letter.offsetHeight * 0.6588 && heart2.parentElement) {
+      heart2.style.display = 'block'
+    }
+    if (-t >= letter.offsetHeight * 0.1988 && heart3.parentElement && !first.parentElement) {
+      heart3.style.display = 'block'
+    }
+
+    if (t <= -letter.offsetHeight + margin) { // when to loop
+      if (first) first.remove()
+      sestina(letter)
+      t = window.innerHeight * 0.7 // reset position
+    }
+  }
+}
+
+// ...................................................................... events
+
+document.querySelectorAll('.q').forEach(heart => {
+  heart.addEventListener('click', (e) => {
+    modal.setAttribute('placeholder', questions[answers.length])
+    modal.style.display = 'block'
+    submit.style.display = 'block'
+    e.target.remove()
+  })
+})
+
+submit.addEventListener('click', () => {
+  const v = modal.value
+  modal.style.display = 'none'
+  submit.style.display = 'none'
+  answers.push(v)
+  modal.value = ''
+  if (answers.length === 3) {
+    const body = `Hello ${answers[0]}, and ${answers[1]} then ${answers[2]}!!!`
+    end(email, subject, body)
+  }
+})
+
+window.addEventListener('load', draw)
